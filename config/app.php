94bc45956faccd79510d64b47ae0e8145cf77ca7
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-06-23 15:28:08 +0300

    Installed and configured Dingo API

--- config/app.php
+++ config/app.php
@@ -136,6 +136,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Dingo\Api\Provider\LaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
